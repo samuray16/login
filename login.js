4bac66df43b2f3af0acb45d467420f8aefac0c1f
SamurayLlara-ux <samurayllara77@gmail.com>
2026-07-16 23:22:25 -0300

    corrigindo o Java Scritp

--- login.js
+++ login.js
@@ -1,22 +1,23 @@
-       function fazerLogin() {
-    const senha = document.getElementById("senha").value; 
-    const notificacao = document.getElementById("notificacao");
-    const email = document.getElementById("email")
-    const placa = document.getElementById("placa")
-
-     
-    if (email.trim ||placa.trim || senha.trim() === "") {
-        notificacao.textContent = "Por favor, preencha os campos vazios";
-        notificacao.style.color = "orange";
-        return; 
-    }
+function fazerLogin() {
     
+    const senha = document.getElementById("senha").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const placa = document.getElementById("placa").value.trim();
+    const notificacao = document.getElementById("notificacao");
+
+   
+    if (email === "" || placa === "" || senha === "") {
+        notificacao.textContent = "Por favor, preencha todos os campos vazios";
+        notificacao.style.color = "orange";
+        return;
+    }
+   
+   
     if (senha === "12345") {
-        notificacao.textContent = "Acesso permitido!";
+        notificacao.textContent = "Senha correta!!!";
         notificacao.style.color = "green";
-        
     } else {
-        notificacao.textContent = "Acesso negado! Senha incorreta.";
+        notificacao.textContent = "Senha incorreta!!!";
         notificacao.style.color = "red";
     }
 }
